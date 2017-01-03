3708bc8b037e92c5f7c059cf0202a02d1bb5f116
sergey <serik-drozd@mail.ru>
2017-01-03 16:17:17 +0200

    + Investigation

---
+++ common/models/Investigation.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%investigation}}".
+ *
+ * @property integer $id
+ * @property integer $company_id
+ * @property string $start_date
+ * @property string $end_date
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ *
+ * @property Company $company
+ */
+class Investigation extends AbstractUndeletableActiveRecord
+{
+    const STATUS_DELETED = 0;
+    const STATUS_CANCELLED = 100;
+    const STATUS_IN_HISTORY = 200;
+    const STATUS_IN_PROGRESS = 300;
+    const STATUS_COMPLETED = 400;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%investigation}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['company_id', 'created_at', 'updated_at'], 'required'],
+            [['company_id', 'status', 'created_at', 'updated_at'], 'integer'],
+            [['start_date', 'end_date'], 'safe'],
+            [['company_id'], 'exist', 'skipOnError' => true, 'targetClass' => Company::className(), 'targetAttribute' => ['company_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'company_id' => 'Company ID',
+            'start_date' => 'Start Date',
+            'end_date' => 'End Date',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCompany()
+    {
+        return $this->hasOne(Company::className(), ['id' => 'company_id']);
+    }
+}
