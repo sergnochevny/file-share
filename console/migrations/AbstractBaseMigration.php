57444e15ebb8077eaffed350479472546aeaf797
sergey <serik-drozd@mail.ru>
2017-01-03 14:49:31 +0200

    + file

---
+++ console/migrations/AbstractBaseMigration.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace console\migrations;
+/**
+ * Class AbstractBaseMigration
+ *
+ * For check if db is MySQL for adjust settings
+ */
+abstract class AbstractBaseMigration extends \yii\db\Migration
+{
+    /**
+     * @var null|string
+     */
+    protected $tblOptions;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $this->tblOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+    }
+}
