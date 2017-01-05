f5e16729e379e6e2530e660c09a67670ae31ced1
sergey <serik-drozd@mail.ru>
2017-01-05 16:45:01 +0200

    added partials layouts

---
+++ backend/views/layouts/tpl/sidebar.php
@@ -0,0 +1,64 @@
+<div class="layout-sidebar">
+    <div class="layout-sidebar-backdrop"></div>
+    <div class="layout-sidebar-body">
+        <div class="custom-scrollbar">
+            <nav id="sidenav" class="sidenav-collapse collapse">
+                <ul class="sidenav">
+                    <li class="sidenav-search hidden-md hidden-lg">
+                        <form class="sidenav-form" action="/">
+                            <div class="form-group form-group-sm">
+                                <div class="input-with-icon">
+                                    <input class="form-control" type="text" placeholder="Search…">
+                                    <span class="icon icon-search input-icon"></span>
+                                </div>
+                            </div>
+                        </form>
+                    </li>
+                    <li class="sidenav-heading">Navigation</li>
+                    <li class="sidenav-item active">
+                        <a href="index.php">
+                            <span class="sidenav-icon icon icon-home"></span>
+                            <span class="sidenav-label">Home</span>
+                        </a>
+                    </li>
+                    <li class="sidenav-item">
+                        <a href="company.php">
+                            <span class="badge badge-success">155</span>
+                            <span class="sidenav-icon icon icon-contao"></span>
+                            <span class="sidenav-label">Companies</span>
+                        </a>
+                    </li>
+                    <li class="sidenav-item">
+                        <a href="investigation.php">
+                            <span class="badge badge-warning" title="Active">13</span>
+                            <span class="badge badge-danger" title="Cancelled">2</span>
+                            <span class="sidenav-icon icon icon-folder-open-o"></span>
+                            <span class="sidenav-label">Applicants</span>
+                        </a>
+                    </li>
+                    <li class="sidenav-item">
+                        <a href="history.php">
+                            <span class="badge badge-success" title="History">87</span>
+                            <span class="sidenav-icon icon icon-history"></span>
+                            <span class="sidenav-label">History</span>
+                        </a>
+                    </li>
+                    <li class="sidenav-item">
+                        <a href="files.php">
+                            <span class="sidenav-icon icon icon-save"></span>
+                            <span class="sidenav-label">Files</span>
+                        </a>
+                    </li>
+                    <li class="sidenav-item">
+                        <a href="agents.php">
+                            <span class="badge badge-success" title="Users">25</span>
+                            <span class="sidenav-icon icon icon-users"></span>
+                            <span class="sidenav-label">Users</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
