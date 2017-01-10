59af6101cf234329099fe7ee886003e10c07e7ee
sergey <serik-drozd@mail.ru>
2017-01-10 12:41:58 +0200

    #1

--- backend/views/layouts/main-login.php
+++ backend/views/layouts/main-login.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-\backend\assets\AppAsset::register($this);
+\backend\assets\CustomAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
