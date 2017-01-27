9a28a469b253a7032b262e1b79fda6ea50e392a9
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-01-27 17:08:16 +0200

    +ext-lib

--- backend/views/file/index.php
+++ backend/views/file/index.php
@@ -16,7 +16,7 @@ $this->title = 'Files';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<?php Pjax::begin(['id' => 'file_search', 'enablePushState' => false, 'scrollTo' => 0, 'timeout' => 0]); ?>
+<?php Pjax::begin(['id' => 'file_search', 'enablePushState' => false, 'timeout' => 0]); ?>
 
 <?php if (Yii::$app->request->isAjax && Yii::$app->session->hasFlash('alert')): ?>
     <?= Alert::widget([
@@ -56,7 +56,7 @@ $this->params['breadcrumbs'][] = $this->title;
                             [
                                 'id' => "upload-file",
                                 'method' => 'post',
-                                'action' => Url::to(['/file/upload'], true),
+                                'action' => ['/file/upload'],
                                 'options' => ['data-pjax' => true]
                             ]
                         ); ?>
