f1a88146701a45e89e3d7fee421ece9fb087deb5
greg <greg.georg.popov@gmail.com>
2017-01-28 11:06:18 +0200

    Refactor

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="user-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?= $this->render('partials/_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
