cc3b0cb5372fe24411aae1a6113b26ff39d4cbd0
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-01-29 15:02:19 +0200

    access rules

--- backend/views/file/partials/_search.php
+++ backend/views/file/partials/_search.php
@@ -17,26 +17,27 @@ use yii\widgets\ActiveForm;
     ],
     'options' => ['class' => 'row']
 ]); ?>
-<div class="col-sm-6">
-    <?= $form->field($model, 'name', [
-        'options' => [
-            'class' => 'dataTables_length'
-        ],
-        'template' => '<label>Show {input} entries</label>',
-    ])->dropDownList(FileSearch::$output_size, [
-        'class' => 'form-control input-sm',
-        'placeholder' => 'Search',
-    ])->label(false) ?>
-</div>
-<div class="col-sm-6">
-    <?= $form->field($model, 'name', [
-        'options' => [
-            'class' => 'dataTables_filter'
-        ]
-    ])->textInput([
-        'class' => 'form-control input-sm',
-        'placeholder' => 'Search',
-    ])->label(false) ?>
-</div>
+    <div class="col-sm-6">
+        <?= $form->field($model, 'pagesize', [
+            'options' => [
+                'class' => 'dataTables_length',
+                'id' => 'pagesize',
+                'data-submit' => true
+            ],
+            'template' => '<label>Show {input} entries</label>',
+        ])->dropDownList(FileSearch::$output_size, [
+            'class' => 'form-control input-sm',
+            'placeholder' => 'Search',
+        ])->label(false) ?>
+    </div>
+    <div class="col-sm-6">
+        <?= $form->field($model, 'name', [
+            'options' => [
+                'class' => 'dataTables_filter'
+            ]
+        ])->textInput([
+            'class' => 'form-control input-sm',
+            'placeholder' => 'Search',
+        ])->label(false) ?>
+    </div>
 <?php ActiveForm::end(); ?>
-
