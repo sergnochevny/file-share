4d3e2feffeda4cb2b52d07a6936d8472d7c507f8
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-01-29 15:55:23 +0200

    search fix

--- backend/views/company/index.php
+++ backend/views/company/index.php
@@ -28,17 +28,12 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="panel-heading">
                 <?= Html::a(Html::tag('span', Html::tag('span', '', ['class' => 'icon icon-plus icon-lg icon-fw']), ['class' => 'btn-label']) . ' Create a new company', Url::to(['/wizard/company']), ['class' => 'btn btn-sm btn-labeled arrow-success']) ?>
             </div>
-            <?php Pjax::begin(['options' => ['class' => 'panel-body panel-collapse']]); ?>
+            <?php Pjax::begin(['options' => ['id'=>'company', 'class' => 'panel-body panel-collapse']]); ?>
                 <?= $this->render('partials/_search', ['model' => $searchModel]); ?>
                 <?= GridView::widget([
                     'dataProvider' => $dataProvider,
                     'tableOptions' => ['class' => 'table table-hover table-striped  dataTable no-footer dtr-inline'],
                     'summaryOptions' => ['class' => 'col-sm-6'],
-                    'pager' => [
-                        'options' => [
-                            'class' => 'col-sm-6',
-                        ]
-                    ],
                     'options' => ['class' => 'row'],
                     'layout'=>"<div class='col-sm-12'>{items}</div>\n{summary}{pager}",
                     'columns' => [
