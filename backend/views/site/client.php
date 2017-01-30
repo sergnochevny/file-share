b1b263af6be191e01a6bf5369bd94f573e85461a
greg <greg.georg.popov@gmail.com>
2017-01-30 17:57:35 +0200

    Front-page

---
+++ backend/views/site/client.php
@@ -0,0 +1,113 @@
+<div class="panel">
+    <div class="panel-body p-a-lg">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="images/logo.png" alt="Plan. Protect. Prosper." width="184" >
+                <div class="p-a">
+                    <strong>Protus</strong>
+                    <br> 2366 Noriega St 726 XXXX New York, NY 10007 United States of America
+                    <br> <a href="tel:+1 234-56789-0123">+1 234-56789-0123</a>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <img class="profile-avetar-img" src="images/user-foto.jpg" alt="Teddy Wilson"  height="87">
+                <div class="p-a">
+                    <strong>Teddy Wilson</strong>
+                    <br> 726 XXXX New York, NY 10007 United States of America
+                    <br> <a href="tel:+1 234-56789-0123">+1 234-56789-0123</a>
+                </div>
+            </div>
+        </div>
+
+        <table id="demo-datatables-responsive-modal" class="table table-hover table-striped  dataTable" cellspacing="0" width="100%">
+            <thead>
+            <tr>
+                <th>Company Name</th>
+                <th>Applicant</th>
+                <th>End date</th>
+                <th>Start date</th>
+                <th>Status</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Stark Industries</td>
+                <td>Sherlock Holmes</td>
+                <td><span class="label label-danger">09.03.2016</span></td>
+                <td><span class="label label-danger">11.03.2016</span></td>
+                <td><span class="label label-danger"><span class="icon icon-stop-circle"></span> Cancelled</span></td>
+                <td>
+                    <a href="wizard.php#editInvestigation" class="btn btn-primary btn-xs" type="button"><span class="icon icon-pencil-square-o "></span> Edit</a>
+                    <a href="#" class="btn btn-danger btn-xs" type="button" data-toggle="modal" data-target="#myModalDelete"><span class="icon icon-trash-o  "></span> Delete</a>
+                    <a href="client_details-investigation.php" class="btn btn-success btn-xs" type="button"> Details</a>
+                </td>
+            </tr>
+            <tr>
+                <td>Stark Industries</td>
+                <td>Sherlock Holmes, Hercule Puaro</td>
+                <td><span class="label label-warning">10.03.2016</span></td>
+                <td><span class="label label-warning">08.03.2016</span></td>
+                <td><span class="label label-warning"><span class="icon icon-refresh"></span> In progress</span></td>
+                <td>
+                    <a href="wizard.php#editInvestigation" class="btn btn-primary btn-xs" type="button"><span class="icon icon-pencil-square-o "></span> Edit</a>
+                    <a href="#" class="btn btn-danger btn-xs" type="button" data-toggle="modal" data-target="#myModalDelete"><span class="icon icon-trash-o  "></span> Delete</a>
+                    <a href="client_details-investigation.php" class="btn btn-success btn-xs" type="button"> Details</a>
+                </td>
+            </tr>
+            <tr>
+                <td>Stark Industries</td>
+                <td>James Bond</td>
+                <td><span class="label label-success">06.03.2016</span></td>
+                <td><span class="label label-success">07.03.2016</span></td>
+                <td><span class="label label-success"><span class="icon icon-check-square-o"></span> Completed</span></td>
+                <td>
+                    <a href="wizard.php#editInvestigation" class="btn btn-primary btn-xs" type="button"><span class="icon icon-pencil-square-o "></span> Edit</a>
+                    <a href="#" class="btn btn-danger btn-xs" type="button" data-toggle="modal" data-target="#myModalDelete"><span class="icon icon-trash-o  "></span> Delete</a>
+                    <a href="client_details-investigation.php" class="btn btn-success btn-xs" type="button"> Details</a>
+                </td>
+            </tr>
+            <tr>
+                <td>Stark Industries</td>
+                <td>Sherlock Holmes</td>
+                <td><span class="label label-danger">10.03.2016</span></td>
+                <td><span class="label label-danger">08.03.2016</span></td>
+                <td><span class="label label-danger"><span class="icon icon-stop-circle"></span> Cancelled</span></td>
+                <td>
+                    <a href="wizard.php#editInvestigation" class="btn btn-primary btn-xs" type="button"><span class="icon icon-pencil-square-o "></span> Edit</a>
+                    <a href="#" class="btn btn-danger btn-xs" type="button" data-toggle="modal" data-target="#myModalDelete"><span class="icon icon-trash-o  "></span> Delete</a>
+                    <a href="client_details-investigation.php" class="btn btn-success btn-xs" type="button"> Details</a>
+                </td>
+            </tr>
+            <tr>
+                <td>Stark Industries</td>
+                <td>Sherlock Holmes, Hercule Puaro</td>
+                <td><span class="label label-warning">10.03.2016</span></td>
+                <td><span class="label label-warning">03.03.2016</span></td>
+                <td><span class="label label-warning"><span class="icon icon-refresh"></span> In progress</span></td>
+                <td>
+                    <a href="wizard.php#editInvestigation" class="btn btn-primary btn-xs" type="button"><span class="icon icon-pencil-square-o "></span> Edit</a>
+                    <a href="#" class="btn btn-danger btn-xs" type="button" data-toggle="modal" data-target="#myModalDelete"><span class="icon icon-trash-o  "></span> Delete</a>
+                    <a href="client_details-investigation.php" class="btn btn-success btn-xs" type="button"> Details</a>
+                </td>
+            </tr>
+            <tr>
+                <td>Stark Industries</td>
+                <td>James Bond</td>
+                <td><span class="label label-success">10.03.2016</span></td>
+                <td><span class="label label-success">11.03.2016</span></td>
+                <td><span class="label label-success"><span class="icon icon-check-square-o"></span> Completed</span></td>
+                <td>
+                    <a href="wizard.php#editInvestigation" class="btn btn-primary btn-xs" type="button"><span class="icon icon-pencil-square-o "></span> Edit</a>
+                    <a href="#" class="btn btn-danger btn-xs" type="button" data-toggle="modal" data-target="#myModalDelete"><span class="icon icon-trash-o  "></span> Delete</a>
+                    <a href="client_details-investigation.php" class="btn btn-success btn-xs" type="button"> Details</a>
+                </td>
+            </tr>
+
+
+
+            </tbody>
+        </table>
+
+    </div>
+</div>
