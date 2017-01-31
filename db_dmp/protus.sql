65c2e9f6e955e48101955edcb8915312ce677ee8
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-01-31 11:59:36 +0200

    db dump

--- db_dmp/protus.sql
+++ db_dmp/protus.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `auth_assignment` (
   CONSTRAINT `auth_assignment_ibfk_1` FOREIGN KEY (`item_name`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы protus.auth_assignment: ~3 rows (приблизительно)
+-- Дамп данных таблицы protus.auth_assignment: ~2 rows (приблизительно)
 /*!40000 ALTER TABLE `auth_assignment` DISABLE KEYS */;
 INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
 	('admin', '2', 1485692238),
@@ -125,16 +125,34 @@ CREATE TABLE IF NOT EXISTS `file` (
   PRIMARY KEY (`id`),
   KEY `type` (`type`),
   KEY `parent` (`parent`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы protus.file: ~4 rows (приблизительно)
+-- Дамп данных таблицы protus.file: ~5 rows (приблизительно)
 /*!40000 ALTER TABLE `file` DISABLE KEYS */;
 INSERT INTO `file` (`id`, `name`, `description`, `size`, `parent`, `type`, `citrix_id`, `created_at`, `updated_at`, `status`) VALUES
 	(7, 'AllFiles', 'Shared files root directory', 0, 'root', 'folder', 'fob4f466-3fd1-4d95-b567-8ac02bdd1eef', 1485600094, 1485600094, 100),
 	(8, 'drag_img1.jpg', '', 7178, 'fob4f466-3fd1-4d95-b567-8ac02bdd1eef', 'jpg', 'fif44d98-d427-f689-0d3c-2ba2245767a1', 1485609057, 1485613263, 100),
 	(9, 'drag_img2.jpg', '', 13681, 'fob4f466-3fd1-4d95-b567-8ac02bdd1eef', 'jpg', 'fi47e9a7-a84a-a3ed-75d8-200d41353e84', 1485613024, 1485613118, 100),
-	(10, '126_1455808234.jpg', '', 16107, 'fob4f466-3fd1-4d95-b567-8ac02bdd1eef', 'jpg', 'fi99851b-71db-49d6-6922-73e0379bed75', 1485693241, 1485693241, 100);
+	(10, '126_1455808234.jpg', '', 16107, 'fob4f466-3fd1-4d95-b567-8ac02bdd1eef', 'jpg', 'fi99851b-71db-49d6-6922-73e0379bed75', 1485693241, 1485693241, 100),
+	(11, 'drag_img1.jpg', '', 7178, 'fo1ec085-e06f-4680-90ed-ca4da2f41fa7', 'jpg', 'fid8fe30-1ce7-478f-e058-248802b662ff', 1485800345, 1485800388, 100);
 /*!40000 ALTER TABLE `file` ENABLE KEYS */;
+
+-- Дамп структуры для таблица protus.history
+DROP TABLE IF EXISTS `history`;
+CREATE TABLE IF NOT EXISTS `history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT '0',
+  `parent` int(11) DEFAULT NULL,
+  `type` varchar(10) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent` (`parent`),
+  KEY `type` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы protus.history: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `history` DISABLE KEYS */;
+/*!40000 ALTER TABLE `history` ENABLE KEYS */;
 
 -- Дамп структуры для таблица protus.investigation
 DROP TABLE IF EXISTS `investigation`;
@@ -155,12 +173,12 @@ CREATE TABLE IF NOT EXISTS `investigation` (
   PRIMARY KEY (`id`),
   KEY `idx-investigation-company_id` (`company_id`),
   CONSTRAINT `fk-investigation-company_id` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- Дамп данных таблицы protus.investigation: ~1 rows (приблизительно)
 /*!40000 ALTER TABLE `investigation` DISABLE KEYS */;
 INSERT INTO `investigation` (`id`, `company_id`, `start_date`, `end_date`, `name`, `description`, `contact_person`, `phone`, `email`, `status`, `created_at`, `updated_at`, `citrix_id`) VALUES
-	(1, 19, '2017-01-31', '2017-01-31', 'Test', 'Test', 'fdsagasdf', '6546345', 'dsfhg@dskfjh.df', 250, 1485617937, 1485617937, 'fo261afb-858a-48a6-933b-f05163e0831f');
+	(2, 19, '0000-00-00', '0000-00-00', 'Test_App', 'Test Applicant', 'rdskflgh sdl;gfk', '4657687', 'kdsjfh@dfjdhf.hj', 250, 1485796515, 1485796515, 'fo1ec085-e06f-4680-90ed-ca4da2f41fa7');
 /*!40000 ALTER TABLE `investigation` ENABLE KEYS */;
 
 -- Дамп структуры для таблица protus.key_storage
@@ -207,7 +225,7 @@ CREATE TABLE IF NOT EXISTS `migration` (
   PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы protus.migration: ~10 rows (приблизительно)
+-- Дамп данных таблицы protus.migration: ~11 rows (приблизительно)
 /*!40000 ALTER TABLE `migration` DISABLE KEYS */;
 INSERT INTO `migration` (`version`, `apply_time`) VALUES
 	('m000000_000000_base', 1485278008),
@@ -219,7 +237,8 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 	('m170103_130522_create_user_company_table', 1485278023),
 	('m170103_134918_create_investigation_table', 1485278024),
 	('m170105_124849_create_logs_table', 1485278024),
-	('m170125_174712_add_citrix_id_column_to_company_and_investigation_tables', 1485419792);
+	('m170125_174712_add_citrix_id_column_to_company_and_investigation_tables', 1485419792),
+	('m170130_172207_user_profile', 1485855928);
 /*!40000 ALTER TABLE `migration` ENABLE KEYS */;
 
 -- Дамп структуры для таблица protus.user
@@ -266,6 +285,26 @@ CREATE TABLE IF NOT EXISTS `user_company` (
 INSERT INTO `user_company` (`user_id`, `company_id`) VALUES
 	(4, 19);
 /*!40000 ALTER TABLE `user_company` ENABLE KEYS */;
+
+-- Дамп структуры для таблица protus.user_profile
+DROP TABLE IF EXISTS `user_profile`;
+CREATE TABLE IF NOT EXISTS `user_profile` (
+  `user_id` int(11) unsigned NOT NULL,
+  `first_name` varchar(55) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(55) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `about_me` text COLLATE utf8_unicode_ci,
+  `layout_src` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `avatar_src` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `idx-first_name` (`first_name`),
+  KEY `idx-last_name` (`last_name`),
+  KEY `idx-full_name` (`first_name`,`last_name`),
+  CONSTRAINT `fk-user_profile-user_id-user-id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Дамп данных таблицы protus.user_profile: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `user_profile` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_profile` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
