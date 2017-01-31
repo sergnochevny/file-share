1c466aafeef99692b335a4e3affdaaaa04f6be39
sergey <serik-drozd@mail.ru>
2017-01-31 21:32:21 +0200

    company create template

--- common/mail/admin/companyCreate-html.php
+++ common/mail/admin/companyCreate-html.php
@@ -1,1 +1,10 @@
 <?php
+/** @var $model \backend\models\Company */
+/** @var $identity \backend\models\User */
+
+use yii\helpers\Html;
+
+?>
+
+<p>The <b><?= Html::encode($model->name) ?></b> company was created at
+    <b><?= $model->created_at ?></b> by <b><?= Html::encode($identity->username) ?></b></p>
--- common/mail/admin/companyCreate-text.php
+++ common/mail/admin/companyCreate-text.php
@@ -1,1 +1,6 @@
 <?php
+/** @var $model \backend\models\Company */
+/** @var $identity \backend\models\User */
+?>
+
+The <?= $model->name ?> company was created at <?= $model->created_at ?> by <?= $identity->username ?>
