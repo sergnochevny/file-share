9dbbd31f76b186973ee9035b788d243c5ecb0848
sergey <serik-drozd@mail.ru>
2017-01-31 21:49:37 +0200

    company update - admin template

--- common/mail/admin/companyUpdate-html.php
+++ common/mail/admin/companyUpdate-html.php
@@ -1,1 +1,9 @@
 <?php
+/** @var $model \backend\models\Company */
+/** @var $identity \backend\models\User */
+
+use yii\helpers\Html;
+
+?>
+
+<p>The <b><?= Html::encode($model->name) ?></b> company was updated at <b><?= Yii::$app->formatter->asDatetime($model->updated_at) ?></b> by <b><?= Html::encode($identity->username) ?></b></p>
