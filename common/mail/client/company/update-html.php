f740ba860fb55ee8790ab24349c1081d3d1cbf39
sergey <serik-drozd@mail.ru>
2017-02-01 17:25:31 +0200

    create & update mail templates for client

--- common/mail/client/company/update-html.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** @var $model \backend\models\Company */
-/** @var $identity \backend\models\User */
-
-use yii\helpers\Html;
-
-?>
-
-<p>Yours <b><?= Html::encode($model->name) ?></b> company was updated at <b><?= Yii::$app->formatter->asDatetime($model->updated_at) ?></b> by <b><?= Html::encode($identity->username) ?></b></p>
