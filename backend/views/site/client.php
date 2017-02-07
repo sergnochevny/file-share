8dfda4919d3904f4e31161dc59cd20c6d0e92960
sergey <serik-drozd@mail.ru>
2017-02-07 16:15:54 +0200

    Rename on main page in client area protus to protus 3

--- backend/views/site/client.php
+++ backend/views/site/client.php
@@ -12,7 +12,7 @@ $company = $user->company;
                 <img src="<?= Url::to('@web/images/logo.png'); ?>" alt="Plan. Protect. Prosper."
                      width="184">
                 <div class="p-a">
-                    <strong>Protus</strong>
+                    <strong>Protus 3</strong>
                     <br> 2366 Noriega St 726 XXXX New York, NY 10007 United States of America
                     <br> <a href="tel:+1 234-56789-0123">+1 234-56789-0123</a>
                 </div>
