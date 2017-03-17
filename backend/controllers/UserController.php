b17cfcb4bc2851d39d82a6e0401fd64d20947161
sergey <serik-drozd@mail.ru>
2017-03-17 18:57:08 +0200

    restrcted access to user controller (not wizard)

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace backend\controllers;
 
+use backend\models\services\UserService;
 use Yii;
+use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
@@ -15,23 +17,6 @@ use backend\models\User;
  */
 class UserController extends Controller
 {
-
-    /**
-     * Finds the User model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return User the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = User::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
-
     /**
      * @inheritdoc
      */
@@ -48,6 +33,20 @@ class UserController extends Controller
                 'class' => RememberUrlBehavior::className(),
                 'actions' => ['index', 'wizard'],
             ],
+            [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['index'],
+                        'roles' => ['admin']
+                    ],
+                    [
+                        'allow' => true,
+                        'roles' => ['superAdmin']
+                    ],
+                ]
+            ]
         ];
     }
 
@@ -67,18 +66,6 @@ class UserController extends Controller
     }
 
     /**
-     * Displays a single User model.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
      * Archive an existing User model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param integer $id
@@ -89,13 +76,25 @@ class UserController extends Controller
         $model = $this->findModel($id);
         /** @var UserService $service */
         $service = Yii::createObject(UserService::class, [$model]);
-        if (Yii::$app->user->can('admin')) {
-            $service->archive();
-            Yii::$app->session->setFlash('success', 'Archived successfully');
-        } else {
-            Yii::$app->session->setFlash('error', 'Permission denied');
-        }
+        $service->archive();
+        Yii::$app->session->setFlash('success', 'Archived successfully');
+
         return $this->actionIndex();
     }
 
+    /**
+     * Finds the User model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return User the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = User::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
 }
