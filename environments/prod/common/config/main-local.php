4d1ffa0846c58b14505b9463142380b273731002
Serg Nochevny <serg.nochevny@engineering.ait.com>
2017-03-22 11:29:57 +0200

    env prod config change

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=protus36_dev',
+            'username' => 'protus36_dev',
+            'password' => 'RMA1988!@',
             'charset' => 'utf8',
         ],
         'mailer' => [
