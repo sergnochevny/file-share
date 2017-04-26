205afec17c854a102fa76304d1d23836cd04fbcb
sergey <serik-drozd@mail.ru>
2017-04-26 15:04:21 +0300

    company case number field

---
+++ backend/models/ResetPassword.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace backend\models;
+
+
+class ResetPassword
+{
+    /**
+     * @param string $token
+     * @return bool
+     */
+    public function validateToken($token)
+    {
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function sendNewOne()
+    {
+
+    }
+}
