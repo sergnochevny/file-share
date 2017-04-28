acb6c0bfdb75f46a36809354855f9ca63879c4f8
sergey <serik-drozd@mail.ru>
2017-04-28 15:48:53 +0300

    there are -> there is

--- common/mail/newPassword-html.php
+++ common/mail/newPassword-html.php
@@ -6,6 +6,6 @@
 
 <p>Hello <?= $user->username ?>,</p>
 
-<p>There are your new password: <?= $newPassword ?></p>
+<p>There is your new password: <?= $newPassword ?></p>
 
 <p>Keep it in secure place.</p>
